d48ee5cc81eb6d6af4886c2e6530f220332e236a
Daniel Imfeld <daniel@imfeld.dev>
2025-01-06 22:08:46 -1000

    remove projectTechnologies in favor of storing this in project config file

--- drizzle/0000_graceful_menace.sql
+++ drizzle/0000_graceful_menace.sql
@@ -8,6 +8,28 @@ CREATE TABLE `agent_instances` (
 	FOREIGN KEY (`project_id`) REFERENCES `projects`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
+CREATE TABLE `agent_task_progress` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`agent_task_id` integer,
+	`agent_task_step_id` integer,
+	`logs` text NOT NULL,
+	`model` text NOT NULL,
+	`operation` text NOT NULL,
+	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
+	`updated_at` integer DEFAULT (unixepoch()) NOT NULL,
+	FOREIGN KEY (`agent_task_id`) REFERENCES `agent_tasks`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`agent_task_step_id`) REFERENCES `agent_task_steps`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `agent_task_steps` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`agent_task_id` integer,
+	`title` text NOT NULL,
+	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
+	`updated_at` integer DEFAULT (unixepoch()) NOT NULL,
+	FOREIGN KEY (`agent_task_id`) REFERENCES `agent_tasks`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
 CREATE TABLE `agent_tasks` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`agent_instance_id` text,
@@ -19,27 +41,6 @@ CREATE TABLE `agent_tasks` (
 	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
 	`updated_at` integer DEFAULT (unixepoch()) NOT NULL,
 	FOREIGN KEY (`agent_instance_id`) REFERENCES `agent_instances`(`id`) ON UPDATE no action ON DELETE no action
-);
---> statement-breakpoint
-CREATE TABLE `project_frameworks` (
-	`project_id` integer NOT NULL,
-	`framework` text NOT NULL,
-	PRIMARY KEY(`project_id`, `framework`),
-	FOREIGN KEY (`project_id`) REFERENCES `projects`(`id`) ON UPDATE no action ON DELETE no action
-);
---> statement-breakpoint
-CREATE TABLE `project_states` (
-	`project_id` integer PRIMARY KEY NOT NULL,
-	`status` text NOT NULL,
-	`last_accessed` integer DEFAULT (strftime('%s', 'now')) NOT NULL,
-	FOREIGN KEY (`project_id`) REFERENCES `projects`(`id`) ON UPDATE no action ON DELETE no action
-);
---> statement-breakpoint
-CREATE TABLE `project_technologies` (
-	`project_id` integer NOT NULL,
-	`technology` text NOT NULL,
-	PRIMARY KEY(`project_id`, `technology`),
-	FOREIGN KEY (`project_id`) REFERENCES `projects`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
 CREATE TABLE `projects` (
