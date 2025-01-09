0754cce7999de353a6f4686a2b03b971216147b8
Daniel Imfeld <daniel@imfeld.dev>
2025-01-08 22:06:13 -1000

    add table for scanned files

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,12 +1,20 @@
 import { relations, sql } from 'drizzle-orm';
-import { sqliteTable, text, integer, int, primaryKey, uniqueIndex } from 'drizzle-orm/sqlite-core';
+import {
+  sqliteTable,
+  text,
+  integer,
+  int,
+  primaryKey,
+  uniqueIndex,
+  blob,
+} from 'drizzle-orm/sqlite-core';
 
 export const projects = sqliteTable(
   'projects',
   {
     id: int('id').primaryKey({ autoIncrement: true }),
     name: text('name').notNull(),
-    path: text('path').notNull()
+    path: text('path').notNull(),
   },
   (table) => [uniqueIndex('project_path_idx').on(table.path)]
 );
@@ -14,7 +22,7 @@ export const projects = sqliteTable(
 type AgentStatus = 'IDLE' | 'PLANNING' | 'EXECUTING' | 'TESTING';
 
 export const agentInstances = sqliteTable('agent_instances', {
-  id: int('id').primaryKey({ autoIncrement: true }),
+  id: int('id').primaryKey(),
   active: int('active', { mode: 'boolean' }).notNull().default(true),
   projectId: text('project_id').references(() => projects.id),
   lastCommit: text('last_commit'),
@@ -24,12 +32,12 @@ export const agentInstances = sqliteTable('agent_instances', {
   updatedAt: integer('updated_at', { mode: 'timestamp' })
     .notNull()
     .default(sql`(unixepoch())`)
-    .$onUpdate(() => sql`unixepoch()`)
+    .$onUpdate(() => sql`unixepoch()`),
 });
 
 /** A history of tasks performed by the agent */
 export const agentTasks = sqliteTable('agent_tasks', {
-  id: int('id').primaryKey({ autoIncrement: true }),
+  id: int('id').primaryKey(),
   agentInstanceId: text('agent_instance_id').references(() => agentInstances.id),
   startVcsRef: text('start_vcs_ref').notNull(),
   endVcsRef: text('end_vcs_ref'),
@@ -42,11 +50,11 @@ export const agentTasks = sqliteTable('agent_tasks', {
   updatedAt: integer('updated_at', { mode: 'timestamp' })
     .notNull()
     .default(sql`(unixepoch())`)
-    .$onUpdate(() => sql`unixepoch()`)
+    .$onUpdate(() => sql`unixepoch()`),
 });
 
 export const agentTaskSteps = sqliteTable('agent_task_steps', {
-  id: int('id').primaryKey({ autoIncrement: true }),
+  id: int('id').primaryKey(),
   agentTaskId: int('agent_task_id').references(() => agentTasks.id),
   title: text('title').notNull(),
   createdAt: integer('created_at', { mode: 'timestamp' })
@@ -55,11 +63,11 @@ export const agentTaskSteps = sqliteTable('agent_task_steps', {
   updatedAt: integer('updated_at', { mode: 'timestamp' })
     .notNull()
     .default(sql`(unixepoch())`)
-    .$onUpdate(() => sql`unixepoch()`)
+    .$onUpdate(() => sql`unixepoch()`),
 });
 
 export const agentTaskProgress = sqliteTable('agent_task_progress', {
-  id: int('id').primaryKey({ autoIncrement: true }),
+  id: int('id').primaryKey(),
   agentTaskId: int('agent_task_id').references(() => agentTasks.id),
   agentTaskStepId: int('agent_task_step_id').references(() => agentTaskSteps.id),
   logs: text('logs').notNull(),
@@ -71,7 +79,22 @@ export const agentTaskProgress = sqliteTable('agent_task_progress', {
   updatedAt: integer('updated_at', { mode: 'timestamp' })
     .notNull()
     .default(sql`(unixepoch())`)
-    .$onUpdate(() => sql`unixepoch()`)
+    .$onUpdate(() => sql`unixepoch()`),
+});
+
+export const scannedFiles = sqliteTable('scanned_files', {
+  id: int('id').primaryKey(),
+  projectId: int('project_id').references(() => projects.id),
+  /** Path relative to the project root */
+  path: text('path').notNull(),
+
+  area: text('area'),
+  short_description: text('short_description'),
+  long_description: text('long_description'),
+
+  file_timestamp: integer('file_timestamp', { mode: 'timestamp' }).notNull(),
+  last_minhash: blob('last_minhash'),
+  current_minhash: blob('current_minhash'),
 });
 
 export type Project = typeof projects.$inferSelect;
